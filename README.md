8290ec3b3f7a14e22814a339a9794f3471d83129
kwonchanmi <kwoncm1999@naver.com>
2021-05-09 21:48:53 +0900

    readme.md 수정

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Jandy_ToDoList
+
+<img width="80%" src="https://user-images.githubusercontent.com/52639963/117572516-2dbdbd00-b10e-11eb-9b8f-d746c755dfa5.gif"/>
+
+### opensource
+A Pen created on CodePen.io. Original URL: [https://codepen.io/THEORLAN2/pen/wGJWwv](https://codepen.io/THEORLAN2/pen/wGJWwv).
+[spring boot 게시판 만들기](https://kyuhyuk.kr/article/spring-boot/2020/07/19/Spring-Boot-JPA-MySQL-Board-Write-Post).
--- src/main/java/com/jandy/todolist/controller/Todolistcontroller.java
+++ src/main/java/com/jandy/todolist/controller/Todolistcontroller.java
@@ -2,6 +2,8 @@ package com.jandy.todolist.controller;
 
 import com.jandy.todolist.dto.TodolistDto;
 import com.jandy.todolist.service.TodolistService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ public class Todolistcontroller {
 
 
     private TodolistService todolistService;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
     public Todolistcontroller(TodolistService todolistService) {
@@ -38,7 +41,7 @@ public class Todolistcontroller {
     @PostMapping("/post/{id}")
     public String delete(@PathVariable("id") Long id){
         todolistService.deleteList(id);
-        /*        logger.info("delete");*/
+        logger.warn(String.valueOf(id));
         return "redirect:/";
     }
 
